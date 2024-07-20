5398c0b8b0ef34e77beabda9a88c5ed27dbc3af0
Hotsyn <85835454+LesyaHotsyn@users.noreply.github.com>
2024-07-20 12:47:09 +0300

    update submit

--- src/main.js
+++ src/main.js
@@ -8,24 +8,31 @@ import {
   errorMessage,
 } from './js/render-functions.js';
 
-document.getElementById('searchButton').addEventListener('click', async () => {
-  const query = document.getElementById('searchInput').value.trim();
-  if (query === '') {
-    return;
-  }
-  clearGallery();
-  showLoading();
-  const perPage = 16;
-  try {
-    const images = await fetchImages(query, perPage);
-    if (images.length === 0) {
-      notFoundMessage();
-    } else {
-      displayImages(images);
+//
+
+document
+  .getElementById('searchForm')
+  .addEventListener('submit', async event => {
+    event.preventDefault(); // Зупинити стандартну поведінку форми
+
+    const query = document.getElementById('searchInput').value.trim();
+    if (query === '') {
+      return;
     }
-  } catch (error) {
-    errorMessage(error);
-  } finally {
-    hideLoading();
-  }
-});
+
+    clearGallery();
+    showLoading();
+    const perPage = 16;
+    try {
+      const images = await fetchImages(query, perPage);
+      if (images.length === 0) {
+        notFoundMessage();
+      } else {
+        displayImages(images);
+      }
+    } catch (error) {
+      errorMessage(error);
+    } finally {
+      hideLoading();
+    }
+  });
